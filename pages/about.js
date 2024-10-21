6b3eefd42a4cb34d2d9dbcfc241f99753190ff6a
Boris Kirov <borisasenovkirov@gmail.com>
2024-10-21 16:31:48 +0200

    addon

--- pages/about.js
+++ pages/about.js
@@ -15,113 +15,44 @@ export default function Contact() {
             <Image src="/avatar1.png" width={322} height={334} />
           </div>
           <div className="cs7 ce11 grid">
-            <h2 className="cs1 ce12">
+            <h2 className="cs1 ce12">ABOUT / CONTACT</h2>
+            <small className="cs1 ce12">
               Born and raised in Sofia, Bulgaria and always between Amsterdam
               and Sofia. I care for details, colours and emotions by creating
               imagery across lifestyle, landscape, architecture, still life,
               fashion and other brain stuff ツ.
-            </h2>
-            <div className="flex cs1 ce12">
+            </small>
+            <div className="cs1 ce12">
+              <Link
+                href="mailto:yngblooods@gmail.com?subject=Let's collaborate"
+                className="flex-items-center"
+              >
+                info@boriskirov.photos
+              </Link>
+            </div>
+            <div className="cs1 ce12">
               <Link
                 href="https://www.instagram.com/yng.blds/"
                 target="_blank"
                 className="flex-items-center"
               >
-                <Image
-                  src="/instagram.svg"
-                  className="icon"
-                  width={24}
-                  height={24}
-                />
-              </Link>
-              <Link
-                href="mailto:yngblooods@gmail.com?subject=Let's collaborate"
-                className="flex-items-center"
-              >
-                <Image
-                  src="/mail.svg"
-                  className="icon"
-                  width={24}
-                  height={24}
-                />
-              </Link>
-              <Link
-                href="https://bento.me/yngblds"
-                className="flex-items-center"
-                target="_blank"
-              >
-                <Image
-                  src="/bento.svg"
-                  className="icon"
-                  width={24}
-                  height={24}
-                />
-              </Link>
-              <Link
-                href="https://bento.me/yngblds"
-                className="flex-items-center"
-                target="_blank"
-              >
-                <Image
-                  src="/darkroom.svg"
-                  className="icon"
-                  width={24}
-                  height={24}
-                />
-              </Link>
-              <Link
-                href="https://bento.me/yngblds"
-                className="flex-items-center"
-                target="_blank"
-              >
-                <Image
-                  src="/substack.svg"
-                  className="icon"
-                  width={24}
-                  height={24}
-                />
+                Instagram
               </Link>
             </div>
-          </div>
-          <div className="cs7 ce11">
-            <details closed>
-              <summary>Features, Mentions & Collaborations: </summary>
-              <br />
-              <small>
-                Camera Japan • Dolomite • Ferrino Italy • North Face • #FR2 •
-                That Divine • raredub •The Alchemist • Boldy James • Negative
-                Feed • Art Cage Budapest • EyeShot Magazine • mnfst • Cheetah
-                bikes • The Maker Market • Haarlem City Blog • Atlas Obscura •
-                Upphotographers • instadenbosch • FramePress Magazine • Dutch
-                Analog • Shoot It With Film • 36h studio • Vice • Bored Panda
-              </small>
-              {/* <ul className="features-mentions">
-                <li>Camera Japan</li>
-                <li>Haarlem City Blog</li>
-                <li>Dolomite</li>
-                <li>Ferrino Italy</li>
-                <li>North Face</li>
-                <li>#FR2</li>
-                <li>That Divine</li>
-                <li>Negative Feed</li>
-                <li>Art Cage Budapest</li>
-                <li>EyeShot Magazine</li>
-                <li>mnfst</li>
-                <li>Cheetah bikes</li>
-                <li>The Maker Market</li>
-                <li>Haarlem City Blog</li>
-                <li>Atlas Obscura</li>
-                <li>Upphotographers</li>
-                <li>instadenbosch</li>
-                <li>FramePress Magazine</li>
-                <li>Dutch Analog</li>
-                <li>Shoot It With Film</li>
-                <li>The Film Public</li>
-                <li>36h_studio</li>
-                <li>Vice</li>
-                <li>Bored Panda</li>
-              </ul> */}
-            </details>
+            <div className="cs1 ce12">
+              <details open>
+                <summary>Features, Mentions & Collaborations: </summary>
+                <br />
+                <small>
+                  Camera Japan • Dolomite • Ferrino Italy • North Face • #FR2 •
+                  That Divine • raredub •The Alchemist • Boldy James • Negative
+                  Feed • Art Cage Budapest • EyeShot Magazine • mnfst • Cheetah
+                  bikes • The Maker Market • Haarlem City Blog • Atlas Obscura •
+                  Upphotographers • instadenbosch • FramePress Magazine • Dutch
+                  Analog • Shoot It With Film • 36h studio • Vice • Bored Panda
+                </small>
+              </details>
+            </div>
           </div>
         </div>
       </main>
