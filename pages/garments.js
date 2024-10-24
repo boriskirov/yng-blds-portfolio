33a344cbd737a906a1e781997aa88f4fe23d500c
Boris Kirov <borisasenovkirov@gmail.com>
2024-10-24 15:56:00 +0200

    new images

--- pages/garments.js
+++ pages/garments.js
@@ -15,31 +15,30 @@ export default function Garments() {
       <main className="cs1 ce12">
         <div className="grid">
           <div className="cs1 ce12 grid-container">
-            <Item src="/garments/garments-47.jpg" width={320} height={240} />
+            <Item src="/garments/garments-4.jpg" width={320} height={240} />
             <Item src="/garments/garments-22.png" width={320} height={240} />
             <Item src="/garments/garments-5.png" width={320} height={240} />
             <Item src="/garments/garments-45.png" width={320} height={240} />
             <Item src="/garments/garments-27.gif" width={320} height={240} />
-            <Item src="/garments/garments-4.jpg" width={320} height={240} />
+            <Item src="/garments/garments-47.jpg" width={320} height={240} />
             <Item src="/garments/garments-3.png" width={320} height={240} />
             <Item src="/garments/garments-16.png" width={320} height={240} />
             <Item src="/garments/garments-15.png" width={320} height={240} />
             <Item src="/garments/garments-26.png" width={320} height={240} />
-            <Item src="/garments/garments-9.png" width={320} height={240} />
+            <Item src="/garments/garments-9.jpg" width={320} height={240} />
             <Item src="/garments/garments-11.png" width={320} height={240} />
             <Item src="/garments/garments-12.png" width={320} height={240} />
             <Item src="/garments/garments-13.png" width={320} height={240} />
             <Item src="/garments/garments-43.png" width={320} height={240} />
             <Item src="/garments/garments-36.png" width={320} height={240} />
             <Item src="/garments/garments-23.png" width={320} height={240} />
-            <Item src="/garments/garments-17.png" width={320} height={240} />
-            <Item src="/garments/garments-18.png" width={320} height={240} />
+
             <Item src="/garments/garments-33.png" width={320} height={240} />
             <Item src="/garments/garments-42.png" width={320} height={240} />
             <Item src="/garments/garments-46.jpg" width={320} height={240} />
             <Item src="/garments/garments-24.png" width={320} height={240} />
             <Item src="/garments/garments-21.png" width={320} height={240} />
-            <Item src="/garments/garments-29.png" width={320} height={240} />
+            <Item src="/garments/garments-29.jpg" width={320} height={240} />
             <Item src="/garments/garments-37.png" width={320} height={240} />
             <Item src="/garments/garments-38.png" width={320} height={240} />
             <Item src="/garments/garments-39.png" width={320} height={240} />
