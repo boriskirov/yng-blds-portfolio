1b3f7c24112e00914c668ea17d4bd4d1b26f693d
Boris Kirov <borisasenovkirov@gmail.com>
2024-10-24 17:47:00 +0200

    new life

--- pages/people.js
+++ pages/people.js
@@ -15,18 +15,19 @@ export default function People() {
         <div className="grid">
           <div className="cs1 ce12 grid-container">
             <Item src="/life/life-35.png" width={320} height={240} />
-            <Item src="/life/life-50.png" width={320} height={240} />
+            <Item src="/life/life-68.jpg" width={320} height={240} />
             <Item src="/life/life-38.png" width={320} height={240} />
             <Item src="/life/life-67.png" width={320} height={240} />
             <Item src="/life/life-40.png" width={320} height={240} />
             <Item src="/life/life-53.png" width={320} height={240} />
             <Item src="/life/life-30.png" width={320} height={240} />
-            67
+            <Item src="/life/life-69.jpg" width={320} height={240} />
             <Item src="/life/life-56.png" width={320} height={240} />
             <Item src="/life/life-66.png" width={320} height={240} />
-            <Item src="/life/life-61.jpg" width={320} height={240} />
-            <Item src="/life/life-54.png" width={320} height={240} />
+
+            <Item src="/life/life-50.png" width={320} height={240} />
             <Item src="/life/life-33.png" width={320} height={240} />
+            <Item src="/life/life-70.jpg" width={320} height={240} />
             <Item src="/life/life-45.png" width={320} height={240} />
             <Item src="/life/life-60.jpg" width={320} height={240} />
             <Item src="/life/life-55.png" width={320} height={240} />
@@ -35,7 +36,8 @@ export default function People() {
             <Item src="/life/life-37.jpg" width={320} height={240} />
             <Item src="/life/life-62.jpg" width={320} height={240} />
             <Item src="/life/life-63.jpg" width={320} height={240} />
-            <Item src="/life/life-65.jpg" width={320} height={240} />
+            <Item src="/life/life-54.png" width={320} height={240} />
+
             <Item src="/life/life-12.png" width={320} height={240} />
             <Item src="/life/life-64.jpg" width={320} height={240} />
             <Item src="/life/life-59.png" width={320} height={240} />
@@ -44,6 +46,7 @@ export default function People() {
             <Item src="/life/life-36.png" width={320} height={240} />
             <Item src="/life/life-34.png" width={320} height={240} />
             <Item src="/life/life-51.png" width={320} height={240} />
+            <Item src="/life/life-61.jpg" width={320} height={240} />
           </div>
         </div>
       </main>
