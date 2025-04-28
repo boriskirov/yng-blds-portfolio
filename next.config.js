4b48b90bb6c6465673ab24e1f47b7aa591220845
Boris Kirov <borisasenovkirov@gmail.com>
2025-04-28 18:09:50 +0200

    blog and improements

--- next.config.js
+++ next.config.js
@@ -1,10 +1,20 @@
-module.exports = {
+const nextConfig = {
   async rewrites() {
     return [
       {
         source: "/feed.xml",
         destination: "/api/feed",
       },
+      {
+        source: "/blog-feed.xml",
+        destination: "/api/blog-feed",
+      },
+      {
+        source: "/daynotes-feed.xml",
+        destination: "/api/daynotes-feed",
+      },
     ];
   },
 };
+
+module.exports = nextConfig;
