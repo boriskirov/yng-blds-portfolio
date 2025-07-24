830fe5bc63472f10fcf54e673caf790b9059559c
Boris Kirov <borisasenovkirov@gmail.com>
2025-07-24 15:59:43 +0200

    addons

--- pages/garments.js
+++ pages/garments.js
@@ -15,22 +15,29 @@ export default function Garments() {
         <div className="grid">
           <div className="cs1 ce12 grid-container">
             <Item src="/garments/garments-4.jpg" width={320} height={240} />
+
             <Item src="/garments/garments-22.jpg" width={320} height={240} />
             <Item src="/garments/garments-26.png" width={320} height={240} />
             <Item src="/garments/garments-5.png" width={320} height={240} />
             <Item src="/garments/garments-45.png" width={320} height={240} />
             <Item src="/garments/garments-27.gif" width={320} height={240} />
+            <Item src="/garments/garments-56.png" width={320} height={240} />
             <Item src="/garments/garments-3.png" width={320} height={240} />
             <Item src="/garments/garments-15.png" width={320} height={240} />
+            <Item src="/garments/garments-57.png" width={320} height={240} />
             <Item src="/garments/garments-52.png" width={320} height={240} />
             <Item src="/garments/garments-53.png" width={320} height={240} />
             <Item src="/garments/garments-50.jpg" width={320} height={240} />
             <Item src="/garments/garments-49.jpg" width={320} height={240} />
+            <Item src="/garments/garments-54.png" width={320} height={240} />
+            <Item src="/garments/garments-55.png" width={320} height={240} />
             <Item src="/garments/garments-11.png" width={320} height={240} />
             <Item src="/garments/garments-12.png" width={320} height={240} />
             <Item src="/garments/garments-48.jpg" width={320} height={240} />
             <Item src="/garments/garments-43.png" width={320} height={240} />
-            <Item src="/garments/garments-21.png" width={320} height={240} />
+            <Item src="/garments/garments-58.png" width={320} height={240} />
+            <Item src="/garments/garments-59.png" width={320} height={240} />
+
             <Item src="/garments/garments-42.png" width={320} height={240} />
             <Item src="/garments/garments-46.jpg" width={320} height={240} />
             <Item src="/garments/garments-9.jpg" width={320} height={240} />
@@ -41,8 +48,6 @@ export default function Garments() {
             <Item src="/garments/garments-40.png" width={320} height={240} />
             <Item src="/garments/garments-41.png" width={320} height={240} />
             <Item src="/garments/garments-44.png" width={320} height={240} />
-            <Item src="/garments/garments-54.png" width={320} height={240} />
-            <Item src="/garments/garments-55.png" width={320} height={240} />
           </div>
         </div>
       </main>
