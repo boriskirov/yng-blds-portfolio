a053ccd076a5151045f81d5d62d5377579bd8a98
Boris Kirov <borisasenovkirov@protonmail.com>
2026-01-01 17:28:28 +0100

    parallax

--- components/header.js
+++ components/header.js
@@ -3,7 +3,9 @@ import Link from "next/link";
 export default function Header({ header = true }) {
   return (
     <header
-      className={`cs1 ce12 flex space-between sticky ${header ? "header" : ""}`}
+      className={`z-index cs1 ce12 flex space-between sticky ${
+        header ? "header" : ""
+      }`}
     >
       <div className="cs1 ce12 flex center">
         <Link href="/">
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,27 @@
 import Header from "../components/header";
 import Metadata from "../components/metadata";
 import PreOrderNow from "../components/pre-order-now";
+import Tilt from "react-parallax-tilt";
 
 export default function Home() {
   return (
-    <div className="main-container grid index-cover cover-main">
+    <div>
       <Metadata
         title="Boris Kirov"
         description="Photography & videography"
         image="/avatar.png"
       />
       <Header header={false} />
-      <main className="cs1 ce12"></main>
+      <Tilt
+        style={{ zIndex: "-1" }}
+        tiltMaxAngleX={1.15}
+        tiltMaxAngleY={1.15}
+        scale={1.01}
+      >
+        <div className="main-container grid index-cover cover-main">
+          <main className="cs1 ce12"></main>
+        </div>
+      </Tilt>
     </div>
   );
 }
